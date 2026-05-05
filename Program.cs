0a0488f3d5451d17d81304188249a27182d294e7
agussch21 <agusschmidt99@gmail.com>
2026-05-05 16:12:03 -0300

    parte 1 del trabajo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¿Cómo te llamás?");
+        
+        string? nombre = Console.ReadLine();
+
+        Console.WriteLine("y tu edad cual es?");
+
+        int edad = int.Parse(Console.ReadLine());
+        
+
+        Console.WriteLine("Tu nombre es: " + nombre);
+        Console.WriteLine("tu edad es:"+edad.ToString());
+    }
+}
